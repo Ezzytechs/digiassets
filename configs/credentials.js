126757ddbb9f1495e3770464d6ca817e2a47f10a
Ezzytechs <jameze49@gmail.com>
2025-10-26 17:21:36 +0100

    mail function fixed

--- configs/credentials.js
+++ configs/credentials.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
-const {SITE_EMAIL, SITE_SUPPORT_EMAIL, SITE_PHONE} = process.env;
+const { SITE_EMAIL, SITE_SUPPORT_EMAIL, SITE_PHONE } = process.env;
 
 //frontend url
-const siteURL="http://localhost:3000"
-//"https://digiassets-flax.vercel.app";
+// const siteURL="http://localhost:3000"
 
-const credentials={
-    siteEmail:SITE_EMAIL,
-    supportEmail:SITE_SUPPORT_EMAIL,
-    appUrl:siteURL,
-    phoneNumber:SITE_PHONE,
-    adminName:"DigiAsset Admin",
-    appName:"DigiAssets",
-    loginURL:`${siteURL}/login`,
-    assetPage:`${siteURL}/assets/my-assets`,
-    orderViewPage:`${siteURL}/user/my-dashboard/purchases/all`,
-    orderViewPageAdmin:`${siteURL}/admin/orders/all`,
-    dashboardurl:`${siteURL}/user/my-dashboard`,
-    dashboardurlAdmin:`${siteURL}/user/my-dashboard`,
-    }
+const siteURL = "https://digizzet.vercel.app";
 
-module.exports=credentials
+const credentials = {
+  siteEmail: SITE_EMAIL,
+  supportEmail: SITE_SUPPORT_EMAIL,
+  appUrl: siteURL,
+  phoneNumber: SITE_PHONE,
+  adminName: "DigiAsset Admin",
+  appName: "DigiAssets",
+  loginURL: `${siteURL}/login`,
+  assetPage: `${siteURL}/assets/my-assets`,
+  orderViewPage: `${siteURL}/user/my-dashboard/purchases/all`,
+  orderViewPageAdmin: `${siteURL}/admin/orders/all`,
+  dashboardurl: `${siteURL}/user/my-dashboard`,
+  dashboardurlAdmin: `${siteURL}/user/my-dashboard`,
+};
+
+module.exports = credentials;
